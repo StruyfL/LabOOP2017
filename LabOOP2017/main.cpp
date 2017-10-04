6311b5e1701355ac104a1492910c1d65a5c270b9
Lars Struyf <u0068830@thomasmore.be>
2017-10-04 16:06:38 +0200

    Initial Commit of project. main.cpp

---
+++ LabOOP2017/main.cpp
@@ -0,0 +1,17 @@
+/*
+*	Lab assignment for Object Oriented Programming course 2017.
+*
+*	Lars Struyf - 2017
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello World" << endl;
+
+	cin.get();
+
+	return 0;
+}
